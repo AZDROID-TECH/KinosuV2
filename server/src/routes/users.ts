7435af3a6dacd4cc5b047b1460dadf8e90f94b32
AZDROID <azdroid.tech@hotmail.com>
2025-05-03 21:05:09 +0400

    Kinosu v2.1 | Big Changes | Fixed

--- server/src/routes/users.ts
+++ server/src/routes/users.ts
@@ -1,2 +1,10 @@
+import express from 'express';
+import { authenticateToken } from '../middleware/auth';
+import * as userController from '../controllers/userController';
+
+const router = express.Router();
+
 // Kullanıcı arama rotasını ekle
 router.get('/search', authenticateToken, userController.searchUsers); 
+
+export default router; 
